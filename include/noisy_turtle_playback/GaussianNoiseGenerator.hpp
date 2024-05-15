fb08b8d065df8f41d821123a2375b4b1d50c7be9
real-Sandip-Das <sd13535@outlook.com>
2024-05-16 01:11:48 +0530

    Modified CMakeLists.txt, package.xml and added GaussianNoiseGenerator.hpp

---
+++ include/noisy_turtle_playback/GaussianNoiseGenerator.hpp
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <vector>
+#include <utility>
+#include <random>
+
+namespace noisy_turtle_playback {
+
+/**
+ * @brief Class containing methods to generate 2D Gaussian Noise with given Variances and Covariance
+ * 
+ */
+class GaussianNoiseGenerator
+{
+  public:
+  
+  /**
+   * @brief Construct a new Gaussian Noise Generator object
+   * 
+   * @param var_x Variance of the first quantity (let, x)
+   * @param var_y Variance of the second quantity (let, y)
+   * @param cov_xy Covariance between the two quantities
+   */
+  GaussianNoiseGenerator(double var_x, double var_y, double cov_xy);
+
+  /**
+   * @brief Destroy the Gaussian Noise Generator object
+   * 
+   */
+  virtual ~GaussianNoiseGenerator();
+
+  /**
+   * @brief Generates a Sample point that follows the Bivariate Gaussian Distribution described by `var_x`, `var_y`, and `cov_xy`
+   * 
+   * @return std::pair<double, double> A sample point from the Bivariate Gaussian Distribution described by `var_x`, `var_y` and `cov_xy`
+   */
+  std::pair<double, double> generateGaussianNoise();
+
+  private:
+
+  /// Covariance between the two quantities
+  const double cov_xy_;
+  /// Variance of the first quantity (let, x)
+  const double var_x_;
+  /// Variance of the second quantity (let, y)
+  const double var_y_;
+
+  /// the determinant of the Covariance matrix
+  const double detCovMat_;
+
+  /// @brief \f$ a = \frac{\sigma_x^2}{2\vert\Sigma\vert} \f$ where, \f$ \sigma_x \f$ is the Standard Deviation of \f$ x \f$ and \f$ \vert\Sigma\vert \f$ denote the determinant of the covariance matrix
+  const double a;
+
+  /// @brief \f$ a = \frac{\sigma_y^2}{2\vert\Sigma\vert} \f$ where, \f$ \sigma_y \f$ is the Standard Deviation of \f$ y \f$ and \f$ \vert\Sigma\vert \f$ denote the determinant of the covariance matrix
+  const double b;
+
+  /// @brief \f$ h = \frac{\sigma_{xy}}{2\vert\Sigma\vert} \f$ where, \f$ \sigma_{xy} \f$ is the Covariance of \f$ x \f$ and \f$ y \f$
+  const double h;
+
+  /**
+   * @brief Uses Box-Muller Transform to create a Sample point following the 2D Standard(i.e. having the Identity Matrix as its Covariance Matrix) Normal Distribution
+   * 
+   * @return std::pair<double, double> (the sample point as a pair of doubles in the format \f$ (x, y) \f$ )
+   */
+  std::pair<double, double> bivariateStandardNormal();
+
+  std::uniform_real_distribution<double> uniform_dis_;
+  std::random_device rd_;
+
+  /**
+   * @brief generates a random number between 0 to 1 following an uniform probability density between 0 and 1 (and zero elsewhere)
+   * 
+   * Uses the a std::random_device object fed into a std::uniform_real_distribution<double> object
+   * 
+   * @return the generated random number
+   */
+  double generateRandomUniform0to1();
+};
+
+} /* namespace */
