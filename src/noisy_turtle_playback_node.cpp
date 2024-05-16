738df4e54df083eadb68b632b1436b0a652b12e5
real-Sandip-Das <sd13535@outlook.com>
2024-05-16 14:06:27 +0530

    Added config.yaml, launch file, main function for the Node

--- src/noisy_turtle_playback_node.cpp
+++ src/noisy_turtle_playback_node.cpp
@@ -1,7 +1,7 @@
 /**
  * @file noisy_turtle_playback_node.cpp
  * @author Sandip Das (sd13535@outlook.com)
- * @brief Source file containing main function for running the Noise adder Node
+ * @brief Source file containing main function for running the Noise adder Node (just calls methods of the Node class)
  * @version 0.1
  * @date 2024-05-13
  * 
@@ -10,13 +10,20 @@
  */
 
 #include <ros/ros.h>
+#include "noisy_turtle_playback/NoisyTurtlePlayback.hpp"
 
 /**
- * @brief 
+ * @brief The main function for running the noisy_turtle_playback Node
  * 
  * @return int 
  */
-int main()
+int main(int argc, char** argv)
 {
-    return 0;
+  ros::init(argc, argv, "subscriber");
+  ros::NodeHandle nodeHandle("~");
+
+  noisy_turtle_playback::NoisyTurtlePlayback noiseAdderNode(nodeHandle);
+
+  ros::spin();
+  return 0;
 }
